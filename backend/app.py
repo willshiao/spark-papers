3f894656431ea5cd99407f04f8293dbb92b1af0d
William Shiao <willshiao@gmail.com>
2018-11-11 20:47:06 -0800

    Implement more API endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,56 @@
 from flask import Flask
 from flask import request, abort
 from flask.json import jsonify
+from magic import reassemble, get_mongo, query_matrix
+from flask_cors import CORS
+
 # from magic import svd, sync_data, depickle_matrix, pickle_matrix, recommend, matrix
 app = Flask(__name__)
+CORS(app)
+
 last_user = 0
+mat = reassemble()
+client = get_mongo()
+db = client['spark']
+p_col = db['papers']
+u_col = db['users']
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
+
+@app.route('/papers', methods=['GET', 'POST'])
+def papers():
+    global mat
+
+    if request.method == 'GET':
+        userId = int(request.args.get('id'))
+        target_id = query_matrix(mat, userId)
+        print('Get target_id: {}'.format(target_id))
+        target_paper = p_col.find_one({ 'pseudoId': target_id }, { '_id': 0 })
+        print('Target paper: {}'.format(target_paper))
+        target_paper['id'] = target_paper['pseudoId']
+        print(target_paper)
+        return jsonify(target_paper)
+    else:
+        data = request.get_json(silent=True)
+        userId = int(data['userId'])
+        reportId = int(data['reportId'])
+        score = int(data['score'])
+        mat[userId, reportId] = score
+        u_col.update_one({ 'userId': userId }, { '$push': { 'ratings': [reportId, score] } })
+        return jsonify({ 'success': True })
+
+
 @app.route('/users/get')
 def get_user_id():
     global last_user
+    u_col.insert({ 'userId': last_user })
     last_user += 1
     return jsonify({ "id": last_user - 1 })
+
 
 @app.route('/update', methods=['POST'])
 def update():
--- backend/magic.py
+++ backend/magic.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pymongo
 import configparser as cfg
-from scipy.sparse.linalg import svds, eigs
 from scipy import stats
-from scipy.sparse import random
+from scipy.sparse import random, lil_matrix
 from pymongo import MongoClient
 import pickle, os, signal, sys, copy
+from scipy.sparse.linalg import svds
 
+
+mongo_conn = None
 
 def pickle_matrix(matrix, filename):
     # filename = input("Pickle jar name: ") # the place the pickled data is saved
@@ -21,11 +23,11 @@ def depickle_matrix(filename):
     return pickle.load(fileObj)
 
 
-if os.path.isfile('pickle'):
-    matrix = depickle_matrix('pickle')
-else:
-    print('create matrix')
-    matrix = np.random.rand(100, 1000)
+# if os.path.isfile('pickle'):
+#     matrix = depickle_matrix('pickle')
+# else:
+#     print('create matrix')
+#     matrix = np.random.rand(100, 1000)
 
 
 def connect():
@@ -45,15 +47,45 @@ def sync_data():
     client = connect()
 
 
+def get_mongo():
+    global mongo_conn
+    if mongo_conn == None:
+        return connect()
+    return mongo_conn
+
+
 def reassemble():
-    client = connect()
-    result = numpy.zeros(num_users, num_papers)
-    for i in users:
-        for j in i:
-            result[i.id, j[0]] = j[1]
+    client = get_mongo()
+    db = client['spark']
+    user_col = db['users']
+    papers_col = db['papers']
+    
+    num_papers = papers_col.estimated_document_count()
+    num_users = user_col.estimated_document_count()
+
+    result = lil_matrix((num_users, num_papers))
+
+    for user in user_col.find({}, { 'userId': 1, 'ratings' : 1}).sort('userId', pymongo.ASCENDING):
+        for rating in user['ratings']:
+            result[user['userId'], rating[0]] = rating[1]
+
+    print('Successfully reassembled matrix!')
+    return result
+
+
+def query_matrix(m, userId):
+    l = m[userId, :]
+    u, sigma, vt = svds(m)
+    res = (l @ (vt.T * sigma)) @ vt
+    res = np.reshape(res, (res.shape[1],))
+    arr_max = np.max(res)
+    print('Max score: {}'.format(arr_max))
+    real_ind = np.random.choice(np.where(res == arr_max)[0], 1)
+    return int(real_ind[0])
 
 
 def signal_handler(sig, frame):
     pickle_matrix(matrix, 'pickle')
     sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
+
+# signal.signal(signal.SIGINT, signal_handler)
