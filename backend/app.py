d716e4f4bd466d2d6ae68cbff925ce4010863efc
William Shiao <willshiao@gmail.com>
2018-11-11 19:18:59 -0800

    Add support for getting fake user IDs

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,36 @@
 from flask import Flask
-from flask import request
-from magic import svd, sync_data, depickle_matrix, pickle_matrix, recommend, matrix
+from flask import request, abort
+from flask.json import jsonify
+# from magic import svd, sync_data, depickle_matrix, pickle_matrix, recommend, matrix
 app = Flask(__name__)
+last_user = 0
 
-
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
 
+@app.route('/users/get')
+def get_user_id():
+    global last_user
+    last_user += 1
+    return jsonify({ "id": last_user - 1 })
 
-@app.route("/update", methods=["POST"])
+@app.route('/update', methods=['POST'])
 def update():
-    if request.method == "POST":
-        rating = request.form["rating"]
-        uid = 1#request.cookies.get("uid")
-        recommend(svd(), uid, None, matrix)
+    if request.method == 'POST':
+        rating = request.form['rating']
+        uid = 1 #request.cookies.get('uid')
+        # recommend(svd(), uid, None, matrix)
     else:
         abort(401)
     return rating
 
 
-@app.route("/sync", methods=["GET"])
+@app.route('/sync', methods=['GET'])
 def sync():
-    if request.method == "GET":
-        print("do something")
+    if request.method == 'GET':
+        print('do something')
     else:
         abort(401)
-    return "getting stuff"
+    return 'getting stuff'
+
